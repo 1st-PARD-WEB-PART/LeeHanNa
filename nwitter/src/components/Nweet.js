f063d32d7276ce1e3f87f5fd4c40363e8f0daa9a
LeeHannaa <tiger5391@naver.com>
2023-05-12 21:56:29 +0900

    delete file

--- nwitter/src/components/Nweet.js
+++ nwitter/src/components/Nweet.js
@@ -1,6 +1,7 @@
 import { deleteDoc, doc, updateDoc } from "firebase/firestore";
-import { dbService } from "../fbase";
+import { dbService, storageService } from "../fbase";
 import React, {useState} from "react";
+import { deleteObject, ref } from "firebase/storage";
 
 const Nweet = ({nweetObj, isOwner}) => { //nweetObj받아서 id확인
     const [editing, setEditing] = useState(false); //수정 모드인지 아닌지 T/F
@@ -10,6 +11,8 @@ const Nweet = ({nweetObj, isOwner}) => { //nweetObj받아서 id확인
         const ok = window.confirm("Are you sure you want to delete this nweet?");
         if(ok) {
             await deleteDoc(NweetTextRef); //delete nweet
+            const ulrRef = ref(storageService, nweetObj.attachmentUrl); //url 가져오기
+            await deleteObject(ulrRef); //삭제 (사진 삭제)
         }
     };
     const toggleEditing = () => setEditing((prev) => !prev); //수정 버튼 누르면 editing변수 t/f
