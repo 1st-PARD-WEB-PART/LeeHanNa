e8a430c407a651599d1a5a1e7e9d0797eabb1607
LeeHannaa <tiger5391@naver.com>
2023-05-13 02:14:57 +0900

    delete 오류 수정 - 이미지 없을 때 해당 nweet 삭제 시 에러

--- nwitter/src/components/Nweet.js
+++ nwitter/src/components/Nweet.js
@@ -11,8 +11,10 @@ const Nweet = ({nweetObj, isOwner}) => { //nweetObj받아서 id확인
         const ok = window.confirm("Are you sure you want to delete this nweet?");
         if(ok) {
             await deleteDoc(NweetTextRef); //delete nweet
-            const ulrRef = ref(storageService, nweetObj.attachmentUrl); //url 가져오기
-            await deleteObject(ulrRef); //url을 찾아 파일도 삭제
+            if(nweetObj.attachmentUrl !==""){
+                const ulrRef = ref(storageService, nweetObj.attachmentUrl); //url 가져오기
+                await deleteObject(ulrRef); //url을 찾아 파일도 삭제
+            }
         }
     };
     const toggleEditing = () => setEditing((prev) => !prev); //수정 버튼 누르면 editing변수 t/f
